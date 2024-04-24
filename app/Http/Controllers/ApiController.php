159749d782bb5cce27e581b5c68de02a37b3c07b
developerv2000 <developerv2000@gmail.com>
2024-04-24 15:17:48 +0500

    Demo function added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Mail\SupervisionReport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\View;
 
 class ApiController extends Controller
 {
@@ -30,5 +29,20 @@ class ApiController extends Controller
         // Mail::to('drugsafety@evolet.co.uk')->send(new SupervisionReport($request));
 
         return redirect($request->redirect_url ?: $request->headers->get('referer'));
+    }
+
+    /**
+     * Demo example for copy + paste
+     *
+     * Dont forget to copy public/forms folder into your own project
+     *
+     * Checkout resources/css/app.css for styles and resources/views/demo.blade.php for rendering
+     */
+    public function supervision()
+    {
+        $response = Http::get('http://farmkomnadzor.spey.tj/api/get-view?lang=' . app()->getLocale() . '&redirect_url=' . route('supervision.index'));
+        $supervisionForm = $response->successful() ? $response->body() : null;
+
+        return view('pages.supervision', compact('supervisionForm'));
     }
 }
